4c5b6b5c82435318ec267961fa477f6c123fed3c
espradli <espradli@trinity.edu>
2020-06-14 23:13:16 -0500

    Updates

---
+++ precache-manifest.9f742620d153906ac1d792edfa6608dd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e05175ba826110811d4ce9b43fd059c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d693255d2bf7388d3bc",
+    "url": "/static/css/2.6e5e8985.chunk.css"
+  },
+  {
+    "revision": "3b8cb8c754710e27b477",
+    "url": "/static/css/main.c6d9482a.chunk.css"
+  },
+  {
+    "revision": "4d693255d2bf7388d3bc",
+    "url": "/static/js/2.84a0bb38.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.84a0bb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8cb8c754710e27b477",
+    "url": "/static/js/main.20048976.chunk.js"
+  },
+  {
+    "revision": "67e5ad1c15b4646df729",
+    "url": "/static/js/runtime-main.2774c108.js"
+  },
+  {
+    "revision": "745e9e7e3aaeba05ec4f1d0ef4078ee3",
+    "url": "/static/media/EmersonSpradling_Resume.745e9e7e.pdf"
+  },
+  {
+    "revision": "782e96992ea85f4374adfdaf0e371364",
+    "url": "/static/media/EmersonSpradling_Resume.782e9699.jpg"
+  },
+  {
+    "revision": "8eef6ce70084cc5f57113b9cc3b86608",
+    "url": "/static/media/IMG_2499.8eef6ce7.jpeg"
+  },
+  {
+    "revision": "c4c2a033cc1c79eb7b349295630ebcfa",
+    "url": "/static/media/Screen Shot 2020-05-31 at 1.33.52 PM.c4c2a033.png"
+  },
+  {
+    "revision": "3c97d15f700bf09446e750ec25a412ff",
+    "url": "/static/media/taxiDistanceClustering2.3c97d15f.png"
+  },
+  {
+    "revision": "8192821c7a438b7bc5b42bc88ac248ee",
+    "url": "/static/media/textImg.8192821c.jpeg"
+  }
+]);
